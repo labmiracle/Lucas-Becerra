474cf656a0bacb83ebed131ce4a9692b904da2e5
LB2712 <becerralucas40@gmail.com>
2023-07-29 18:35:02 -0300

    Programacion-Funcional

---
+++ Programacion-Funcional/function-map.js
@@ -0,0 +1,44 @@
+const characters = [
+    { name: 'Luke Skywalker', 
+        height: '172', 
+        mass: '77', 
+        eye_color: 'blue', 
+        gender: 'male' 
+    },
+    { name: 'Darth Vader', 
+        height: '202', 
+        mass: '136', 
+        eye_color: 'yellow', 
+        gender: 'male' 
+    },
+    { name: 'Leia Organa', 
+        height: '150',
+        mass: '49', 
+        eye_color: 'brown', 
+        gender: 'female' 
+    },
+    { name: 'Anakin Skywalker', 
+        height: '188', 
+        mass: '84', 
+        eye_color: 'blue', 
+        gender: 'male' 
+    }
+  ];
+
+//Obtener un arreglo de todos los nombres
+
+  const NamesArray = characters.map(characters => characters.name); 
+  
+  console.log(NamesArray);
+
+//Obtener un arreglo de todas las alturas
+
+  const HeightsArray = characters.map(characters => characters.height);
+
+  console.log (HeightsArray);
+
+//Obtener un arreglo de objetos con las propiedades nombres y alturas  
+
+  const NameAndHeightArray = characters.map(characters => ({name:characters.name, height:characters.height }));
+
+  console.log (NameAndHeightArray);
