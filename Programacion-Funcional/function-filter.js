4d8df76a0ae8b573658128af1f8d44b2cbd8ccba
LB2712 <becerralucas40@gmail.com>
2023-07-29 20:50:57 -0300

    function-filter.js modified

--- Programacion-Funcional/function-filter.js
+++ Programacion-Funcional/function-filter.js
@@ -26,17 +26,25 @@ const characters = [
   ];
 
 //Obtener personajes con masa superior a 100
+
 const charactersWithMassGreaterThan100 = characters.filter(character => parseInt(character.mass) > 100);
-    console.log(charactersWithMassGreaterThan100);
+    
+console.log(charactersWithMassGreaterThan100);
 
 //Obtener personajes con una altura inferior a 200
+
 const charactersWithHeightLessThan200 = characters.filter(character => parseInt(character.height) < 200);
-    console.log(charactersWithHeightLessThan200);
+    
+console.log(charactersWithHeightLessThan200);
 
 //Obtener todos los personajes masculinos
+
 const maleCharacters = characters.filter(character => character.gender === 'male');
-    console.log(maleCharacters);
+    
+console.log(maleCharacters);
 
 //Obtener todos los personajes femeninos
+
 const femaleCharacters = characters.filter(character => character.gender === 'female');
-    console.log(femaleCharacters);
+    
+console.log(femaleCharacters);
