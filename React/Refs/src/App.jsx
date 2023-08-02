86a5b3fc67401b5b24e30d387805912434c24905
LB2712 <becerralucas40@gmail.com>
2023-08-02 16:32:35 -0300

    form done

---
+++ React/Refs/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Form from "./components/text";
+
+function App() {
+  return (
+    <>
+      <h1>Form</h1>
+      <Form>
+
+      </Form>
+    </>
+  );
+}
+
+export default App;
---
+++ React/Refs/src/components/text.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+
+export default function Form() {
+
+  const [InputName, setValorInputName] = useState("");
+  const [InputLastname, setValorInputLastname] = useState("");
+
+  const handleInputChangeName = (event) => {
+    setValorInputName(event.target.value);
+  };
+
+  const handleInputChangeLastname = (event) => {
+    setValorInputLastname(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Input value name:", InputName);
+    console.log("Input value last name:", InputLastname);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        {" "}
+        Name:{" "}
+        <input
+          type="text"
+          value={InputName}
+          onChange={handleInputChangeName}
+        />{" "}
+      </label>
+      <br />
+      <label>
+        {" "}
+        Last Name:{" "}
+        <input
+          type="text"
+          value={InputLastname}
+          onChange={handleInputChangeLastname}
+        />{" "}
+      </label>
+      <br />
+      <button type="submit">Send</button>
+    </form>
+  );
+}
