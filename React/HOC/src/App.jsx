581dea0655cccc3b80b687ff9610397503318aa9
LB2712 <becerralucas40@gmail.com>
2023-08-03 17:06:32 -0300

    App.jsx modified

--- React/HOC/src/App.jsx
+++ React/HOC/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+//HOC-PeticionAPI
+
+const withApiData = (WrappedComponent) => {
+  return () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(
+            "https://jsonplaceholder.typicode.com/posts"
+          );
+          const jsonData = await response.json();
+          setData(jsonData);
+        } catch (error) {
+          console.log("Error fetching data desde API:", error);
+        }
+      };
+
+      fetchData();
+    }, []);
+
+    return <WrappedComponent data={data} />;
+  };
+};
+
+//List posts
+
+const PostList = ({ data }) => {
+  return (
+    <div>
+      <h2>List Posts</h2>
+      {data.map((post) => (
+        <div key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const PostsWithApiData = withApiData(PostList);
+
+export default function App() {
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <PostsWithApiData />
     </>
-  )
+  );
 }
-
-export default App
